9779e80b6c2ab3027572b9bb08401326e121c343
Nils Olsson <nilso@enosis.net>
2021-08-21 23:12:43 -0700

    Lazy commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import { ViteAliases } from 'vite-aliases'
-// import ViteRsw from 'vite-plugin-rsw'
+import ViteRsw from 'vite-plugin-rsw'
 
 export default defineConfig({
     plugins: [
         Vue({ include: [/(?<!\.md)\.vue$/] }),
         ViteAliases(),
-        // ViteRsw({
-        //     crates: [
-        //         'wasm'
-        //     ]
-        // }),
+        ViteRsw({ crates: [ 'wasm' ] }),
     ]
 })
